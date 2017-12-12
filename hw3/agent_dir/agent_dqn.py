c7a494f26ff8874c87b44d7fe50310785c3fa7b1
daniel <ss910278@gmail.com>
2017-12-13 00:56:06 +0800

    upload a bunch of things

---
+++ hw3/agent_dir/agent_dqn.py
@@ -0,0 +1,160 @@
+from agent_dir.agent import Agent
+from collections import deque
+from keras.models import Sequential
+from keras.layers import Lambda, Dense, Activation, GRU, TimeDistributed, Conv1D, MaxPooling1D, MaxPooling2D, Flatten, Dropout, SimpleRNN, Bidirectional, Convolution2D, Permute, BatchNormalization, RepeatVector, Input, MaxPool2D 
+from keras.models import load_model, Model
+from keras.layers import merge, Concatenate, Merge, add, Masking, Activation, dot, multiply, concatenate
+from keras import backend as K
+import tensorflow as tf 
+import numpy as np
+import random
+import time
+class Agent_DQN(Agent):
+    def __init__(self, env, args):
+
+        super(Agent_DQN,self).__init__(env)
+        self.env = env
+        self.state = env.reset()
+        self.action_size = env.action_space
+        self.memory = deque(maxlen=50000)
+        self.gamma = 0.99    # discount rate
+        self.epsilon = 0.001 # exploration rate
+        self.epsilon_min = 0.00001
+        self.epsilon_decay = 0.995
+        self.learning_rate = 0.001
+        self.session = ""
+        self.graph_ops = ""
+        if args.test_dqn:
+            g = tf.Graph()
+            self.session = tf.Session(graph=g)
+            with g.as_default(), self.session.as_default():
+                K.set_session(self.session)
+                self.graph_ops = self.build_graph(3)
+                saver = tf.train.Saver()
+                saver.restore(self.session, "./breakout.ckpt")
+                self.session.run(self.graph_ops["reset_target_network_params"])
+
+    def build_network(self, num_actions, w, h, c):
+        with tf.device("/gpu:0"):
+            state = tf.placeholder("float", [None, w, h, c])
+            inputs = Input(shape=(w, h, c))
+            model = Convolution2D(32, (3,3), activation='relu', padding='same')(inputs)
+            model = MaxPool2D((2,2))(model)
+            model = Convolution2D(64, (3,3), activation='relu', padding='same')(model)
+            model = MaxPool2D((2,2))(model)
+            model = Flatten()(model)
+            model = Dense(512, activation='relu')(model)
+            q_values = Dense(num_actions, activation='linear')(model)
+            m = Model(inputs, q_values)
+        return state, m
+
+    def build_graph(self, num_actions):
+        # Create shared deep q network
+        s, q_network = self.build_network(num_actions, 84, 84, 4)
+        network_params = q_network.trainable_weights
+        q_values = q_network(s)
+
+        # Create shared target network
+        st, target_q_network = self.build_network(num_actions, 84, 84, 4)
+        target_network_params = target_q_network.trainable_weights
+        target_q_values = target_q_network(st)
+
+        # Op for periodically updating target network with online network weights
+        reset_target_network_params = [target_network_params[i].assign(network_params[i]) for i in range(len(target_network_params))]
+        
+        # Define cost and gradient update op
+        a = tf.placeholder("float", [None, num_actions])
+        y = tf.placeholder("float", [None])
+        action_q_values = tf.reduce_sum(q_values * a, reduction_indices=1)
+        cost = tf.reduce_mean(tf.square(y - action_q_values))
+        optimizer = tf.train.AdamOptimizer(0.001)
+        grad_update = optimizer.minimize(cost, var_list=network_params)
+
+        graph_ops = {"s" : s, 
+                     "q_values" : q_values,
+                     "st" : st, 
+                     "target_q_values" : target_q_values,
+                     "reset_target_network_params" : reset_target_network_params,
+                     "a" : a,
+                     "y" : y,
+                     "grad_update" : grad_update}
+        return graph_ops
+
+    def remember(self, state, action, reward, next_state, done):
+        self.memory.append((state, action, reward, next_state, done))
+
+    def init_game_setting(self): 
+        pass
+
+
+    def replay(self, batch_size, session, graph_ops):
+        minibatch = random.sample(self.memory, batch_size)
+        X_state, X_action, Y = [], [], []
+        for state, action, reward, next_state, done in minibatch:
+            target = reward
+            if not done:
+                target = reward + self.gamma * \
+                       np.amax(graph_ops['target_q_values'].eval(session = session, feed_dict = {graph_ops['st'] : [next_state]}))
+
+            action_input = np.zeros(3)
+            action_input[action-1] = 1
+
+            Y.append(target)
+            X_state.append(state)
+            X_action.append(action_input)
+
+        session.run(graph_ops['grad_update'], feed_dict = { graph_ops['y'] : np.asarray(Y),
+                                                            graph_ops['a'] : np.asarray(X_action),
+                                                            graph_ops['s'] : np.asarray(X_state)} )
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
+
+    def train(self):
+        g = tf.Graph()
+        session = tf.Session(graph=g)
+        with g.as_default(), session.as_default():
+            K.set_session(session)
+            graph_ops = self.build_graph(3)
+            saver = tf.train.Saver()
+            #saver.restore(session, "./breakout.ckpt")
+            session.run(tf.global_variables_initializer())
+            session.run(graph_ops["reset_target_network_params"])
+            #writer = tf.train.SummaryWriter('./', session.graph)
+            episodes = 100000
+            
+            for e in range(episodes):
+                
+                env = self.env
+                state = env.reset()
+                total_reward = 0
+                
+                for time_t in range(10000):
+                    action = self.make_action(state, session, graph_ops)
+                    next_state, reward, done, info = self.env.step(action)
+                    total_reward += reward
+                    self.remember(state, action, reward, next_state, done)
+                    state = next_state
+                    if done:
+                        print("episode: {}/{}, score: {}"
+                              .format(e, episodes, total_reward))
+                        break
+                
+                replay = len(self.memory) if len(self.memory) < 64 else 64
+                self.replay(replay, session, graph_ops)
+
+                if e % 100 == 0:
+                    session.run(graph_ops['reset_target_network_params'])
+
+                # if e % 200 == 0:
+                #     saver.save(session, "./breakout.ckpt")
+        pass
+
+    def make_action(self, state, test=True):
+        if test:
+            self.epsilon = 0.01
+
+        if np.random.rand() <= self.epsilon:
+            return random.randrange(3)+1
+
+        action = self.graph_ops['q_values'].eval(session = self.session, feed_dict = {self.graph_ops['s'] : [state]})
+        return np.argmax(action)+1
---
+++ hw3/agent_dir/agent_pg.py
@@ -0,0 +1,143 @@
+from agent_dir.agent import Agent
+from collections import deque
+from keras.models import Sequential
+from keras.layers import Reshape, Lambda, Dense, Activation, GRU, TimeDistributed, Conv1D, MaxPooling1D, MaxPooling2D, Flatten, Dropout, SimpleRNN, Bidirectional, Convolution2D, Permute, BatchNormalization, RepeatVector, Input, MaxPool2D 
+from keras.optimizers import Adam
+from keras.models import load_model, Model
+from keras.layers import merge, Concatenate, Merge, add, Masking, Activation, dot, multiply, concatenate
+from keras import backend as K
+import tensorflow as tf 
+import numpy as np
+import random
+import time
+class Agent_PG(Agent):
+    def __init__(self, env, args):
+        """
+        Initialize every things you need here.
+        For example: building your model
+        """
+        self.state_size = 80*80
+        self.action_size = 3
+        self.gamma = 0.99
+        self.learning_rate = 0.001
+        self.states = []
+        self.gradients = []
+        self.rewards = []
+        self.probs = []
+        self.model = self._build_model()
+        self.model.summary()
+        self.record = deque(maxlen=30)
+        self.prev_state = None
+        #elf.csv = open("pong_baseline_2.csv",'a')
+        super(Agent_PG,self).__init__(env)
+
+        if args.test_pg:
+            self.model = load_model('pong.h5')
+            print('loading trained model')
+
+
+    def init_game_setting(self):
+        pass
+
+    def _build_model(self):
+        model = Sequential()
+        model.add(Convolution2D(32, 6, 6, subsample=(3, 3), input_shape=(80,80,1), border_mode='same',activation='relu', init='he_uniform'))
+        model.add(Flatten())
+        model.add(Dense(64, activation='relu', init='he_uniform'))
+        model.add(Dense(32, activation='relu', init='he_uniform'))
+        model.add(Dense(self.action_size, activation='softmax'))
+        opt = Adam(lr=self.learning_rate)
+        model.compile(loss='categorical_crossentropy', optimizer=opt)
+        return model
+
+    def preprocess(self, I):
+        I = I[35:195]
+        I = I[::2, ::2, 0]
+        I[I == 144] = 0
+        I[I == 109] = 0
+        I[I != 0] = 1
+        I = I.astype(np.float).ravel()
+        return I.reshape((80,80,1))
+
+    def remember(self, state, action, prob, reward):
+        y = np.zeros([self.action_size])
+        y[action] = 1
+        gradient = np.array(y).astype('float32') - prob
+        self.gradients.append(gradient)
+        self.states.append(state)
+        self.rewards.append(reward)
+        # self.memory.append((state, gradient, aprob, reward))
+
+    def act(self, state):
+        state = np.expand_dims(state,axis=0)
+        aprob = self.model.predict(state, batch_size=1).flatten()
+        self.probs.append(aprob)
+        action = np.random.choice(self.action_size, 1, p=aprob)[0]
+        return action, aprob
+
+    def discount_rewards(self, rewards):
+        discounted_rewards = np.zeros_like(rewards)
+        running_add = 0
+        for t in reversed(range(0, rewards.size)):
+            if rewards[t] != 0:
+                running_add = 0
+            running_add = running_add * self.gamma + rewards[t]
+            discounted_rewards[t] = running_add
+        return discounted_rewards
+
+    def train(self):
+
+        env = self.env
+        state = env.reset()
+        score = 0
+        episode = 0
+
+        while True:
+            state = self.preprocess(state)
+            x = state - self.prev_state if self.prev_state is not None else np.zeros(state.shape)
+            self.prev_state = state
+
+            action, prob = self.make_action(x, test=False)
+            state, reward, done, info = env.step(action + 1)
+            score += reward
+            self.remember(x, action, prob, reward)
+            if done:
+                episode += 1
+                gradients = np.vstack(self.gradients)
+                rewards = np.vstack(self.rewards)
+                rewards = self.discount_rewards(rewards)
+                rewards = (rewards - np.mean(rewards)) / np.std(rewards)
+                gradients *= rewards
+                X = np.asarray(self.states)
+                Y = self.probs + self.learning_rate * np.vstack([gradients])
+                self.model.train_on_batch(X, Y)
+                self.record.append(score) 
+                running_reward = np.array(self.record).mean() if len(self.record) <= 30 else np.array(self.record).mean()
+                self.states, self.probs, self.gradients, self.rewards = [], [], [], []
+                print('Episode: %d - Score: %f. - moving average: %f' % (episode, score, running_reward))
+                score = 0
+                state = env.reset()
+                self.prev_state = None
+                if episode > 1 and episode % 20 == 0:
+                    self.model.save('pong.h5')
+
+        pass
+
+
+    def make_action(self, state, test=True):
+        if test :
+            state = self.preprocess(state)
+            x = state - self.prev_state if self.prev_state is not None else np.zeros(state.shape)
+            self.prev_state = state
+            state = np.expand_dims(x,axis=0)
+            aprob = self.model.predict(state, batch_size=1).flatten()
+            action = np.random.choice(self.action_size, 1, p=aprob)[0]
+            return action
+        else:
+            state = np.expand_dims(state,axis=0)
+            aprob = self.model.predict(state, batch_size=1).flatten()
+            self.probs.append(aprob)
+            action = np.random.choice(self.action_size, 1, p=aprob)[0] 
+            return action, aprob
+
+
